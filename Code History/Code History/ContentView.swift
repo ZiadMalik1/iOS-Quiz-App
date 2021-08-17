ad31c43acc5450311ed650e9d6e936eb634274d9
Ziad <zmali002@fiu.edu>
2021-08-17 19:01:03 -0400

    Cleaned Up Code and Added Responsiveness

--- Code History/Code History/ContentView.swift
+++ Code History/Code History/ContentView.swift
@@ -7,10 +7,15 @@
 
 import SwiftUI
 
-let mainColor = Color(red: 20/255, green: 28/255, blue: 58/255);
 
 
 struct ContentView: View {
+    
+    @State var mainColor = Color(red: 20/255, green: 28/255, blue: 58/255);
+
+    
+    let question = Question(questionText: "Who is the Greatest Basketball Player of All Time ?", possibleAnswers: ["Lebron", "Micheal", "Kobe"], correctAnswerIndex: 0)
+    
     let accentColor = Color(red: 48/255, green: 105/255, blue: 240/255);
     var body: some View {
         ZStack{
@@ -20,41 +25,25 @@ struct ContentView: View {
                     .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
                     .padding()
                 Spacer().frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 274, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                Text("Who is the Greatest Basketball Player of All Time ?")
+                Text(question.questionText)
                     .font(.largeTitle)
                     .bold()
                     .multilineTextAlignment(.center)
                     .padding(.horizontal)
                 Spacer()
                 HStack{
-                    Button(action: {
-                        print("Chose 1")
-                    }, label: {
-                        Text("Lebron")
-                            .font(.body)
-                            .bold()
-                            .multilineTextAlignment(.center)
-                            .padding()
-                            .border(accentColor, width: 4)
-                    })
-                    Button(action: {
-                        print("Chose 2")
-                    }, label: {
-                        Text("Kobe").font(.body)
-                            .bold()
-                            .multilineTextAlignment(.center)
-                            .padding()
-                            .border(accentColor, width: 4)
-                    })
-                    Button(action: {
-                        print("Chose 3")
-                    }, label: {
-                        Text("Hakeem").font(.body)
-                            .bold()
-                            .multilineTextAlignment(.center)
-                            .padding()
-                            .border(accentColor, width: 4)
-                    })
+                    
+                    ForEach(0..<question.possibleAnswers.count) { answerIndex in
+                        
+                        Button(action: {
+                            print("Tapped On Option With Text: \(question.possibleAnswers[answerIndex])")
+                            mainColor = answerIndex == question.correctAnswerIndex ? .green : .red
+                        }, label: {
+                            ChoiceTextView(choiceText: question.possibleAnswers[answerIndex])
+                        })
+                        
+                    }
+                    
                 }
                 Spacer().frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
             }
---
+++ Code History/Code History/Question.swift
@@ -0,0 +1,16 @@
+//
+//  Question.swift
+//  Code History
+//
+//  Created by Ziad Malik on 8/17/21.
+//
+
+import Foundation
+
+struct Question {
+    
+    var questionText: String
+    var possibleAnswers: [String]
+    var correctAnswerIndex: Int
+    
+}
